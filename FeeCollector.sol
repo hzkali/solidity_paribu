685b4a398b71174b7224fa5ade9fcf4f51694e8a
Enesyellice <68306319+hzkali@users.noreply.github.com>
2022-12-22 19:26:41 +0300

    paribu

---
+++ FeeCollector.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.17;
+
+contract FeeCollector {
+    // we are declaring owner variable as address type
+    address public owner;
+    // we are declaring balance of the owner as unsigned integer type
+    uint256 public balance;
+
+    // there we have a constructor function to start the contract
+    constructor() {
+        owner = msg.sender; // 0x3BF0ff163136Cd4D3211773ed593d15f179a0f66
+    }
+
+    // this function increases balance by value of 'msg.value'
+    receive() external payable {
+        balance += msg.value;
+    }
+
+    // this function sends ETH to given address
+    function withdraw(uint256 amount, address payable destAddr) public {
+        // throws error if msg.sender is not owner
+        require(msg.sender == owner, "Only owner can withdraw");
+        // throws error if amount > balance of the account
+        require(amount <= balance, "Insufficient funds");
+        // after being sure about the function,
+        // we are sending eth to the given address
+        destAddr.transfer(amount);
+        // decreasing balance by value of the sent amount
+        balance -= amount;
+    }
+}
